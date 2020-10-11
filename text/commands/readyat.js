f1416dba801ede60d7120c72e6116ed2e4f8821e
Ben Esposito <benpesposito@gmail.com>
2020-10-11 01:20:41 -0400

    Fixed bugs discovered with new readyat

--- text/commands/readyat.js
+++ text/commands/readyat.js
@@ -7,119 +7,144 @@ module.exports = {
     param: 'time',
     secret: false,
 	description: "Declares yourself as ready, but in the future",
+	execute
+}
+
+function execute(message, args, bot) {
+	let arg = args.join(' ');
 	
-    execute(message, args, bot) {
-		console.log(args);
-		let arg = args.join(' ');
-		
-		if(!arg) {
-			message.channel.send('Try entering a time');
-		} else {
-			let readyTime = this.parseTime(arg);
-
-			if(readyTime) {
-				if(arg == 'cancel') {
-					if(bot.readySoon.get(message.member.id)) {
-						message.channel.send(`${message.member.displayName} will no longer be ready at ${this.getTimeString(readyTime)}`);
-						bot.readySoon.delete(message.member.id);
-					}
-				} else {
-					message.channel.send(`I've got you marked down for ${this.getTimeString(readyTime)}`);
-
-					/* -------- thanks Josiah ---------- */
-					let currentTime = new Date();
-					let readyAtMillis = (readyTime.hour * 60 + readyTime.minute) * 60 * 1000;
-					let currentMillis = ((currentTime.getHours() * 60 + currentTime.getMinutes()) * 60 + currentTime.getSeconds()) * 1000;
-
-					let delay = readyAtMillis - currentMillis;
-					
-					if(delay < 0)
-						delay += (24 * 60 * 60 * 1000);
-					/* --------------------------------- */
-
-					setTimeout(function() {
-						message.channel.send(`Are ya ready yet, <@${message.member.id}>?`);
-						bot.readySoon.delete(message.member.id);
-						this.savetoFile(bot);
-					}, delay);
-					
-				}
-
-				message.react('✅');
-				bot.readySoon.set(message.member.id, [message.member.id, readyTime.hour, readyTime.minute]);
-				this.savetoFile(bot);
-			} else
-				message.channel.send('Failed to parse time, try again');
-		}
-	},
-
-	parseTime(timeString) {
-		let matches = /(\d+)(?::(\d+))?(?:\s*(am|pm))?$/.exec(timeString);
-		
-		if(matches) {
-			let time = {
-				hour: Number(matches[1]),
-				minute: Number(matches[2])
+	if(!arg)
+		message.channel.send('Try entering a time');
+	else if(arg == 'cancel') {
+		if(bot.readySoon.get(message.member.id)) {
+			time = {
+				hour: bot.readySoon.get(message.member.id)[1],
+				minute: bot.readySoon.get(message.member.id)[2]
 			}
 
+			message.channel.send(`${message.member.displayName} will no longer be ready at ${getTimeString(time)}`);
+			
+			bot.readySoon.delete(message.member.id);
+			savetoFile(bot);
+		}
+	} else {
+		let readyTime = parseTime(arg);
+
+		if(readyTime) {
+			message.channel.send(`I've got you marked down for ${getTimeString(readyTime)}`);
+
+			/* -------- thanks Josiah ---------- */
+			let currentTime = new Date();
+			let readyAtMillis = (readyTime.hour * 60 + readyTime.minute) * 60 * 1000;
+			let currentMillis = ((currentTime.getHours() * 60 + currentTime.getMinutes()) * 60 + currentTime.getSeconds()) * 1000;
+
+			let delay = readyAtMillis - currentMillis;
+			
+			if(delay < 0)
+				delay += (24 * 60 * 60 * 1000);
+			/* --------------------------------- */
+
+			setTimeout(function() {
+				message.channel.send(`Are ya ready yet, <@${message.member.id}>?`);
+				bot.readySoon.delete(message.member.id);
+				savetoFile(bot);
+			}, delay);
+			
+			message.react('✅');
+			bot.readySoon.set(message.member.id, [message.member.id, readyTime.hour, readyTime.minute]);
+			savetoFile(bot);
+		} else
+			message.channel.send('Failed to parse time, try again');
+	}
+}
+
+function parseTime(timeString) {
+	let time;
+	let matches;
+	
+	if(matches = /^(\d{1,2})(?::(\d{2}))?(?:\s*(am|pm))?$/.exec(timeString)) {
+		time = {
+			hour: Number(matches[1]),
+			minute: Number(matches[2])
+		}
+
+		if(time.hour > 12)
+			time = null;
+		else {
 			let meridian = matches[3];
 
 			if(!time.minute)
 				time.minute = 0;
-			
+
 			if(meridian) {
-				if(meridian == 'pm')
+				if(meridian == 'pm' && time.hour != 12)
 					time.hour += 12;
 			} else {
 				let currentTime = new Date();
 
-				if(!(currentTime.getHours() % 12 > time.hour ||
-						currentTime.getHours() % 12 == time.hour && currentTime.getMinutes() > time.minute))
+				if(currentTime.getHours() % 12 > time.hour ||
+						(currentTime.getHours() % 12 == time.hour && currentTime.getMinutes() > time.minute)) {
 					time.hour += 12;
+				}
 			}
 
 			time.hour %= 24;
+		}
+	}
+	
+	if(!time && (matches = /^(\d{2}):?(\d{2})$/.exec(timeString))) {
+		time = {
+			hour: Number(matches[1]),
+			minute: Number(matches[2])
+		}
+	}
 
-			return time;
-		} else
-			return null;
-	},
+	if(!time)
+		return null;
 
-	getTimeString(time) {
-		let meridian = (time.hour >= 12)? 'pm' : 'am';
-		let hourStr = time.hour % 12;
+	if(time.hour >= 24)
+		return null;
+	
+	if(time.minute >= 60)
+		return null;
 
-		if(hourStr == '0')
-			hourStr = '12';
-		
-		return `${hourStr % 12}:${String(time.minute).padStart(2, '0')}${meridian}`;
-	},
+	return time;
+}
 
-    savetoFile: function (bot)
-    {
-        const FileSystem = require("fs");
+function getTimeString(time) {
+	let meridian = (time.hour >= 12)? 'pm' : 'am';
+	let hourStr = time.hour % 12;
 
-        var wrapper =
-        {
-            readyAtList: []
-        }
+	if(hourStr == '0')
+		hourStr = '12';
+	
+	return `${hourStr}:${String(time.minute).padStart(2, '0')}${meridian}`;
+}
 
-        //converts the collection of readyat times to an array of readyat times
-        bot.readySoon.forEach(thing =>
-        {
-            wrapper.readyAtList.push(thing);
-        });
+function savetoFile(bot)
+{
+	const FileSystem = require("fs");
 
-        var fileName = 'readyAtList.json';
-        if (bot.testbuild)
-        {
-            fileName = 'testReadyAtList.json';
-        }
+	var wrapper =
+	{
+		readyAtList: []
+	}
 
-        //saves the array to a file
-        FileSystem.writeFile(fileName, JSON.stringify(wrapper), e =>
-        {
-            if (e) throw e;
-        });
-    }
+	//converts the collection of readyat times to an array of readyat times
+	bot.readySoon.forEach(thing =>
+	{
+		wrapper.readyAtList.push(thing);
+	});
+
+	var fileName = 'readyAtList.json';
+	if (bot.testbuild)
+	{
+		fileName = 'testReadyAtList.json';
+	}
+
+	//saves the array to a file
+	FileSystem.writeFile(fileName, JSON.stringify(wrapper), e =>
+	{
+		if (e) throw e;
+	});
 }
