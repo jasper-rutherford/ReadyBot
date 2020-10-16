a1024929b333b46ec88219a0af741eb00792b312
Ben Esposito <benpesposito@gmail.com>
2020-10-15 21:25:36 -0400

    Updated NPM license and added config.json to setup script

--- setup.sh
+++ setup.sh
@@ -2,3 +2,12 @@ npm i
 
 echo '{"readyAtList":[]}' > testReadyAtList.json
 echo '{"readyAtList":[]}' > readyAtList.json
+
+(
+cat << END
+{
+    "token": "",
+    "testToken": ""
+}
+END
+) > config.json
