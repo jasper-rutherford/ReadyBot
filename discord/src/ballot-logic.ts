56e264963d7a798349b71084a9699d71b7dd2479
Jasper Rutherford <jasperrutherford874@gmail.com>
2026-02-07 17:41:25 -0600

    ballot logic awaits an api client.

--- discord/src/ballot-logic.ts
+++ discord/src/ballot-logic.ts
@@ -11,7 +11,7 @@ import {
   SHITBOT_USER_ID,
   UTILITY_BALLOT_EMOJI,
   VOTE_BALLOT_EMOJI,
-} from "./env";
+} from "./env.js";
 
 export function detectBallots(
   reaction: MessageReaction | PartialMessageReaction,
@@ -22,12 +22,14 @@ export function detectBallots(
     return;
   }
 
+  let ballotType = "";
+
   // check if emoji is ballot1 or ballot2
   if (reaction.emoji.name === mustGetEnv(UTILITY_BALLOT_EMOJI)) {
-    return "utility";
+    ballotType = "utility";
   } else if (reaction.emoji.name === mustGetEnv(VOTE_BALLOT_EMOJI)) {
-    return "vote";
+    ballotType = "vote";
+  } else {
+    return;
   }
-
-  return;
 }
